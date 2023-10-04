0d78859e9738a45f41883e241436786e5d49aa28
Kevin Loquencio <kevin.loquencio@lanexcorp.com>
2023-10-04 16:23:14 +0800

    add wktohml

--- app/Helpers/Exports/ExportFile.php
+++ app/Helpers/Exports/ExportFile.php
@@ -3,7 +3,9 @@
 namespace App\Helpers\Exports;
 
 use App\Models\Loan;
+use Exception;
 use Illuminate\Support\Facades\Storage;
+use mikehaertl\wkhtmlto\Pdf;
 use Spatie\Browsershot\Browsershot;
 
 class ExportFile {
@@ -13,24 +15,21 @@ class ExportFile {
         $Storage->makeDirectory($path);
         $path = $path. $filename;
         $storagePath = $Storage->path($path);
-     
-        // Transform to pdf
-        Browsershot::html($view)
-                ->newHeadless()
-                ->noSandbox()
-                ->emulateMedia('print')
-                ->margins(10, 10, 10, 10)
-                ->setNodeBinary(config('browsershot.node_path'))
-                ->setNpmBinary(config('browsershot.npm_path'))
-                ->setChromePath(config('browsershot.chrome_path'))
-                ->savePdf($storagePath);   
         
+        $pdf = new Pdf(array(
+            'binary' => '/home/whtmcawt/etc/dspacc.com/wktohtml/local/bin/wkhtmltopdf',
+        ));
+        $pdf->addPage($view);
+
+        if (!$pdf->saveAs($storagePath))
+            throw new Exception($pdf->getError(), 1);
+
         return $Storage->url($path);
     }
 
     public static function exportAgreement(Loan $loan) {
  
-        $view = view('exports.loans.agreement', compact('loan'));
+        $view = view('exports.loans.agreement', compact('loan'))->render();
 
         $filename = $loan->member->full_name . "-" . $loan->loan_number . ".pdf";
         $path =  "loans/$loan->id/agreements/";
