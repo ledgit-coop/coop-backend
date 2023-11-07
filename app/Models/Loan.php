a2797b782595f7b2daf3f76f9a9a3bfedef9be1c
Kevin Loquencio <kevin.loquencio@lanexcorp.com>
2023-11-07 12:24:07 +0800

    add loan casting

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -82,6 +82,8 @@ class Loan extends Model
         'released_date' => 'datetime:Y-m-d',
         'next_payroll_date' => 'datetime:Y-m-d',
         'released' => 'boolean',
+        'number_of_children' => 'integer',
+        'age' => 'integer'
     ];
     
     // Define the foreign key relationships to the Member and LoanProduct models
