812fe8f18ff8c68974093d330d62e46c5f5013ab
Kevin Loquencio <kevin.loquencio@lanexcorp.com>
2023-11-25 23:34:11 +0800

    fix account transaction duplication

--- app/Console/Commands/FixAccountTransactionDuplication.php
+++ app/Console/Commands/FixAccountTransactionDuplication.php
@@ -30,12 +30,12 @@ class FixAccountTransactionDuplication extends Command
     {
         DB::unprepared("
             DROP TABLE if exists TempTable;
-            CREATE TEMPORARY TABLE TempTable ( SELECT w.*, ROW_NUMBER() OVER (PARTITION BY w.particular, w.transaction_date, w.amount ORDER BY w.id) AS row_num FROM account_transactions AS w JOIN account_transactions AS z ON z.particular = w.particular AND z.transaction_date = w.transaction_date AND z.amount = w.amount AND z.member_account_id = w.member_account_id AND z.id > w.id );
+            CREATE TEMPORARY TABLE TempTable ( SELECT w.*, ROW_NUMBER() OVER (PARTITION BY w.particular, w.transaction_date, w.amount ORDER BY w.id) AS row_num FROM account_transactions AS w JOIN account_transactions AS z ON z.particular = w.particular AND z.transaction_date = w.transaction_date AND z.amount = w.amount AND z.member_account_id = w.member_account_id AND z.id > w.id where w.deleted_at is null);
             update account_transactions w JOIN TempTable t ON t.id = w.id and t.row_num > 1 set w.deleted_at = now();
             DROP TABLE if exists TempTable;
 
             DROP TABLE if exists TempTable;
-            CREATE TEMPORARY TABLE TempTable ( SELECT w.*, ROW_NUMBER() OVER (PARTITION BY w.particular, w.transaction_date, w.amount ORDER BY w.id) AS row_num FROM transactions AS w JOIN transactions AS z ON z.particular = w.particular AND z.transaction_date = w.transaction_date AND z.amount = w.amount AND z.transaction_sub_type_id = w.transaction_sub_type_id AND z.id > w.id );
+            CREATE TEMPORARY TABLE TempTable (SELECT w.*, ROW_NUMBER() OVER (PARTITION BY w.particular, w.transaction_date, w.amount ORDER BY w.id) AS row_num FROM transactions AS w JOIN transactions AS z ON z.particular = w.particular AND z.transaction_date = w.transaction_date AND z.amount = w.amount AND z.transaction_sub_type_id = w.transaction_sub_type_id AND z.id > w.id where w.deleted_at is null and w.type <> 'expense');
             update transactions w JOIN TempTable t ON t.id = w.id and t.row_num > 1 set w.deleted_at = now();
             DROP TABLE if exists TempTable;
         ");
